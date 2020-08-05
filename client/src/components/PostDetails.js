1709c1f7068a9375fc6438233e32479d006e884d
Mahmoud Osman <mahmoudosman.som@gmail.com>
2020-08-04 23:03:28 -0500

    added products page

--- client/src/components/PostDetails.js
+++ client/src/components/PostDetails.js
@@ -3,11 +3,11 @@ import React, { Component } from "react";
 export default class PostDetails extends Component {
   render() {
     return (
-      <div className="container">
+      <section className="container">
         <h1>Post Details</h1>
         <hr />
         <p className="text-danger">Comming Soon!!</p>
-      </div>
+      </section>
     );
   }
 }
